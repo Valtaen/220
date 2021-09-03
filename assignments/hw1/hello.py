80caaa5211e2c4a92f4b7eb8ecfede353129df64
Valtaen <stewartms2@g.cofc.edu>
2021-09-02 22:38:51 -0400

    Completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,14 @@
+"""
+Name: Matt Stewart
+hello.py
+
+Problem: Create a function that prints "Hello, world!" while testing to ensure I can properly upload and sync files to Github
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+def main():
+    print("hello, world!")
+
+#main()
+#to allow autorunning upon loading the program as opposed manually calling.
