0f8f1385b2c1ae754db9ee50d55ae062805ef6b0
Valtaen <stewartms2@g.cofc.edu>
2021-09-22 18:23:41 -0400

    HQ3 Completed

--- assignments/hw3/mean.py
+++ assignments/hw3/mean.py
@@ -14,20 +14,22 @@ def main():
     n = eval(input("Enter the number of values to be entered: "))
     rms_temp = 0
     harm_temp = 0
-    geo_temp = 0
+    geo_temp = 1
     tmp = 0
     for i in range(n):
         tmp = eval(input("Enter value " + str(i+1) + ": "))
         rms_temp = rms_temp + (tmp**2)
         harm_temp = harm_temp + (1 / tmp)
-        geo_temp = geo_temp + (1 * tmp)
+        geo_temp = geo_temp * tmp
     rms_mean = math.sqrt(rms_temp / n)
-    harm_mean = harm_temp
-    for i in range(n):
+    harm_mean = n / harm_temp
+    for i in range(1,n-1):
         geo_temp = math.sqrt(geo_temp)
-#RMS= sqrt of arithmetic mean of squares of n inputs, should be 6.205
+"""
+RMS= sqrt of arithmetic mean of squares of n inputs, should be 6.205
 #harmonic mean = 1 divided by sum of 1/n1+1/n2+1/n3+etc, should be 4.0
 #geometric mean = nth root of the sum product of n, should be 4.729
+"""
     print(round(rms_mean, 3))
     print(round(harm_mean, 3))
     print(round(geo_temp, 3))
