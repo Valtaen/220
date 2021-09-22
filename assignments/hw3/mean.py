6c47caac6ac7c447439bb483eb41da3df0eff52f
Valtaen <stewartms2@g.cofc.edu>
2021-09-22 15:23:32 -0400

    Completed lab4, finishing hq3

---
+++ assignments/hw3/mean.py
@@ -0,0 +1,36 @@
+"""
+Name: Matt Stewart
+mean.py
+
+Problem: Given user input, output a set of three means (RMS, harmonic, and geometric)
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+import math
+
+def main():
+    n = eval(input("Enter the number of values to be entered: "))
+    rms_temp = 0
+    harm_temp = 0
+    geo_temp = 0
+    tmp = 0
+    for i in range(n):
+        tmp = eval(input("Enter value " + str(i+1) + ": "))
+        rms_temp = rms_temp + (tmp**2)
+        harm_temp = harm_temp + (1 / tmp)
+        geo_temp = geo_temp + (1 * tmp)
+    rms_mean = math.sqrt(rms_temp / n)
+    harm_mean = harm_temp
+    for i in range(n):
+        geo_temp = math.sqrt(geo_temp)
+#RMS= sqrt of arithmetic mean of squares of n inputs, should be 6.205
+#harmonic mean = 1 divided by sum of 1/n1+1/n2+1/n3+etc, should be 4.0
+#geometric mean = nth root of the sum product of n, should be 4.729
+    print(round(rms_mean, 3))
+    print(round(harm_mean, 3))
+    print(round(geo_temp, 3))
+
+if __name__ == '__main__':
+    main()
--- labs/lab4/lab4.py
+++ labs/lab4/lab4.py
@@ -1,9 +1,15 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Matt Stewart
+lab4.py
+
+Problem: Various area, radius, and pi functions
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
 """
 
 from graphics import *
+import math
 
 
 def squares():
@@ -30,25 +36,28 @@ def squares():
 
     # create a space to instruct user
     inst_pt = Point(width / 2, height - 10)
-    instructions = Text(inst_pt, "Click to move circle")
+    instructions = Text(inst_pt, "Click to set a square, then click to add squares")
     instructions.draw(win)
 
-    # builds a circle
-    shape = Circle(Point(50, 50), 20)
+    # builds a square
+    cen = win.getMouse()
+    shape = Rectangle(Point(cen.getX()-10, cen.getY()-10), Point(cen.getX()+10,cen.getY()+10))
     shape.setOutline("red")
     shape.setFill("red")
     shape.draw(win)
 
-    # allows the user to click multiple times to move the circle
+    # allows the user to click multiple times to copy the square
     for i in range(num_clicks):
         p = win.getMouse()
-        c = shape.getCenter()  # center of circle
+        shape = Rectangle(Point(p.getX() - 10, p.getY() -10), Point(p.getX() + 10, p.getY() +10))
+        shape.setOutline("red")
+        shape.setFill("red")
+        shape.draw(win)
 
-        # move amount is distance from center of circle to the
-        # point where the user clicked
-        dx = p.getX() - c.getX()
-        dy = p.getY() - c.getY()
-        shape.move(dx, dy)
+    instructions.undraw()
+    inst_pt = Point(width / 2, height - 10)
+    instructions = Text(inst_pt, "Click to close the window.")
+    instructions.draw(win)
 
     win.getMouse()
     win.close()
@@ -62,14 +71,81 @@ def rectangle():
          Print the perimeter and area of the rectangle.
     Formulas: area = (length)(width)   and    perimeter = 2(length+width)
     """
+    width = 400
+    height = 400
+    win = GraphWin("Lab 4", width, height)
+
+    p1 = win.getMouse()
+    p2 = win.getMouse()
+    r = Rectangle(p1, p2)
+    r.setOutline("blue")
+    r.setFill("blue")
+    r.draw(win)
+    l = abs(p1.getX() - p2.getX())
+    w = abs(p1.getY() - p2.getY())
+    area = l * w
+    perimeter = 2*(l + w)
+
+    inst_pt = Point(width / 2, 390)
+    instructions = Text(inst_pt, "The area is: " + str(area))
+    instructions.draw(win)
+
+    inst_pt = Point(width / 2, 370)
+    instructions = Text(inst_pt, "The perimeter is: " + str(perimeter))
+    instructions.draw(win)
+
+    win.getMouse()
+    win.close()
+
     pass
+
+
+def circle():
+    width = 400
+    height = 400
+    win = GraphWin("Lab 4", width, height)
+
+    cen = win.getMouse()
+    x = cen.getX()
+    y = cen.getY()
+    rad = win.getMouse()
+    xx = rad.getX()
+    yy = rad.getY()
+    r = (abs(xx - x) ** 2) + (abs(yy - y)**2)
+    r2 = math.sqrt(r)
+    circ = Circle(Point(x, y), r2)
+    circ.setFill("green")
+    circ.draw(win)
+
+
+
+    inst_pt = Point(width / 2, 370)
+    instructions = Text(inst_pt, "The radius is: " + str(r2))
+    instructions.draw(win)
+
+    win.getMouse()
+    win.close()
+
+
+    pass
+
+def pi2():
+    n = eval(input("Enter the number of terms to sum: "))
+    acc = 0
+    for i in range(n):
+        num = 4
+        den = 1 + 2*i
+        frac = (num/den)*((-1)**i)
+        acc = acc + frac
+    print(acc)
+    print(math.pi - acc)
 
 
 def main():
     squares()
-    # rectangle()
-    # circle()
-    # pi2()
+    rectangle()
+    circle()
+    pi2()
 
 
 main()
