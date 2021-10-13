4b972b8e283269ebf9663df8ae0f8ea4b3bbd524
Valtaen <stewartms2@g.cofc.edu>
2021-10-13 14:40:19 -0400

    Lab7

--- labs/lab7/lab7.py
+++ labs/lab7/lab7.py
@@ -1,12 +1,68 @@
 """
-Name: <your name goes here – first and last>
-Partner: <your partner's name goes here – first and last>
-<ProgramName>.py
-"""
+Name: Matt stewart
 
+lab7.py
+"""
+import math
+
+def cash_conversion():
+    num = eval(input("Enter an integer: "))
+    print("$" + '{:.2f}'.format(num))
+
+def encode():
+    s = input('Enter your message: ')
+    key = eval(input('Enter your key: '))
+    acc = " "
+    for r in s:
+        x = ord(r)
+        x = x + key
+        acc = acc + chr(x)
+    print(acc)
+
+def sphere_area(radius):
+    area = 4 * math.pi * (radius ** 2)
+    print(area)
+
+def sphere_volume(radius):
+    volume = 4/3 * math.pi * (radius ** 3)
+    print(volume)
+
+def sum_n(n):
+    acc = 0
+    for i in range(n):
+        acc = acc + i
+    print(acc)
+    return (acc)
+
+def sum_n_cubes(n):
+    acc = 0
+    for i in range(n):
+        acc = acc + i**3
+    print(acc)
+    return(acc)
+
+def encode_better():
+    s = input("Enter your message: ")
+    k = input("Enter your key: ")
+    acc= " "
+    for i in range(len(s)):
+        c = s[i]
+        c = ord(c)
+        key = k[i%len(k)]
+        key = ord(key)-97
+        c = c + key
+        c = chr(c)
+        acc = acc + c
+    print(acc)
 
 def main():
-    # add function calls here
+    cash_conversion()
+    encode()
+    sphere_area(n)
+    sphere_volume(n)
+    sum_n(n)
+    sum_n_cubes(n)
+    encode_better()
     pass
 
 
